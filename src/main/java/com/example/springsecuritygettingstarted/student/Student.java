09347fce7527b9784f37fde36dcf88ccac1f647d
Ivaylo Traykov <ivaylotraykov027@gmail.com>
2021-06-22 22:30:34 +0300

    adding first controller

---
+++ src/main/java/com/example/springsecuritygettingstarted/student/Student.java
@@ -0,0 +1,19 @@
+package com.example.springsecuritygettingstarted.student;
+
+public class Student {
+    private final Long studentId;
+    private final String studentName;
+
+    public Student(Long studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+}
